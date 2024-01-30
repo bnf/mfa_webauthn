1fe3506ab310925f5a7523e5b9e65ff5c1c81c43
Benjamin Franzke <ben@bnf.dev>
2024-01-30 10:14:54 +0100

    Mark compatible with TYPO3 v13.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
-            'typo3' => '11.2.0-12.4.99',
+            'typo3' => '11.2.0-13.0.99',
             'php' => '8.1.0-8.2.99'
         ],
         'suggests' => [
