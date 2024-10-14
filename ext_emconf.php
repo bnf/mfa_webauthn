8f80f436a1b7a83bc982a5cf4b106bf588e7688b
Benjamin Franzke <ben@bnf.dev>
2024-10-14 11:36:49 +0200

    Mark compatible with TYPO3 v13.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
-            'typo3' => '11.2.0-13.0.99',
-            'php' => '8.1.0-8.2.99'
+            'typo3' => '11.2.0-13.4.99',
+            'php' => '8.1.0-8.4.99'
         ],
         'suggests' => [
             'sf_yubikey' => '*',
