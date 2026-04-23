53273534c1d119c921582b7143565c4f59b9fdb7
Benjamin Franzke <ben@bnf.dev>
2026-04-23 12:31:17 +0200

    Require at least PHP 8.2 for webauhn-lib
    webauthn-lib uses readonly classes

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
-            'typo3' => '11.2.0-14.3.99',
-            'php' => '8.1.0-8.5.99'
+            'typo3' => '12.0.0-14.3.99',
+            'php' => '8.2.0-8.5.99'
         ],
         'suggests' => [
             'sf_yubikey' => '*',
